587e437d96cfda31e59beb4a08511ff38f1b013b
Grzesiek1 <chwiluk.grzegorz@gmail.com>
2017-04-22 16:40:12 +0200

    [Updated] Extension of sample database

--- example.sql
+++ example.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 22 Kwi 2017, 01:19
+-- Czas generowania: 22 Kwi 2017, 16:38
 -- Wersja serwera: 10.1.13-MariaDB
 -- Wersja PHP: 7.0.8
 
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `TreesNavigation`
+-- Baza danych: `treesnavigation`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `Trees`
+-- Struktura tabeli dla tabeli `trees`
 --
 
 CREATE TABLE `trees` (
@@ -34,13 +34,43 @@ CREATE TABLE `trees` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
+-- Zrzut danych tabeli `trees`
+--
+
+INSERT INTO `trees` (`id`, `name`, `parent`, `display_order`) VALUES
+(1, 'a', 0, 1),
+(2, 'b', 0, 2),
+(3, 'c', 0, 3),
+(4, 'ba', 2, 1),
+(5, 'bb', 2, 2),
+(6, 'bc', 2, 3),
+(7, 'd', 0, 4),
+(8, 'e', 0, 5),
+(9, 'f', 0, 6),
+(10, 'bba', 5, 1),
+(11, 'bbb', 5, 2),
+(12, 'bbc', 5, 3),
+(13, 'bbd', 5, 4),
+(14, 'bbca', 12, 1),
+(15, 'bbcb', 12, 2),
+(16, 'bbcc', 12, 3),
+(17, 'bbcd (sort: display_order - test)', 12, 5),
+(18, 'bbce (sort: display_order - test)', 12, 4),
+(19, 'da', 7, 1),
+(20, 'db', 7, 2),
+(21, 'dc', 7, 3),
+(22, 'dba', 20, 1),
+(23, 'dbb', 20, 2),
+(24, 'dbc', 20, 3);
+
+--
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `Trees`
+-- Indexes for table `trees`
 --
-ALTER TABLE `Trees`
+ALTER TABLE `trees`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -48,10 +78,10 @@ ALTER TABLE `Trees`
 --
 
 --
--- AUTO_INCREMENT dla tabeli `Trees`
+-- AUTO_INCREMENT dla tabeli `trees`
 --
-ALTER TABLE `Trees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `trees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
